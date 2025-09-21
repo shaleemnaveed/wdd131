beb9b7e188e956b905b286985d69201fcd7e3d31
shaleemnaveed <shaleemnaveed@gmail.com>
2025-09-21 20:20:32 +0500

    emoticon added

--- scripts/place.js
+++ scripts/place.js
@@ -2,14 +2,17 @@ const temperature = parseFloat(document.getElementById('temp').textContent);
 const windspeed = parseFloat(document.getElementById('speed').textContent);
 
 const windchill = document.getElementById('windchill');
-windchill.textContent = calculateWindChill(temperature, windspeed);
+if (temperature <= 10 && windspeed >= 4.8) {
+    windchill.textContent = calculateWindChill(temperature, windspeed);
+}
+else {
+    windchill.textContent = "N/A";
+}
+
 
 function calculateWindChill(temperature, windspeed) {
-    if (temperature <= 10 && windspeed >= 4.8) {
-        return 13.12
-            + (0.6215 * temperature)
-            - (11.37 * (windspeed ** 0.16))
-            + (0.3965 * temperature * (windspeed ** 0.16));
-    }
-    return "N/A";
+    return 13.12
+        + (0.6215 * temperature)
+        - (11.37 * (windspeed ** 0.16))
+        + (0.3965 * temperature * (windspeed ** 0.16));
 }
